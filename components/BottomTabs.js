6cc634efb7aaa1b8740bf22ed1eb6f0939e50496
ziyang li <zl@auburn.edu>
2020-05-29 12:47:05 -0500

    now able to swipe between screens

--- components/BottomTabs.js
+++ components/BottomTabs.js
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { Text, View } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
 import FrontBurner from "../screens/FrontBurner";
 import BackBurner from "../screens/BackBurner";
 import Vitamins from "../screens/Vitamins";
@@ -10,7 +12,27 @@ const Tab = createBottomTabNavigator();
 
 export default function BottomTabs() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "FrontBurner") {
+            iconName = focused ? "ios-list-box" : "ios-list";
+          } else if (route.name === "BackBurner") {
+            iconName = focused ? "ios-list-box" : "ios-list";
+          } else if (route.name === "Vitamins") {
+            iconName = focused ? "ios-list-box" : "ios-list";
+          } else if (route.name === "Oven") {
+            iconName = focused ? "ios-list-box" : "ios-list";
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: "tomato",
+        inactiveTintColor: "gray",
+      }}>
       <Tab.Screen name='FrontBurner' component={FrontBurner} />
       <Tab.Screen name='BackBurner' component={BackBurner} />
       <Tab.Screen name='Vitamins' component={Vitamins} />
